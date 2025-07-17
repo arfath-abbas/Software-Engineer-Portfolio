31fd3c32e5c6babe0e12b90a9efe652ef0f308e3
Arfath <arfathabbas.abbas@bt.com>
2025-07-17 23:48:29 +0530

    updated personal project

--- data/index.ts
+++ data/index.ts
@@ -71,32 +71,16 @@ export const projects = [
       "/spring-boot.svg",
       "/aws.svg",
     ],
-    link: "/ui.earth.com",
+    link: "https://finance-tracker-angular-app.vercel.app/",
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    // img: "/p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "/ui.yoom.com",
-  },
-  {
-    id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    // img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "/ui.aiimg.com",
-  },
-  {
-    id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    // img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "/ui.apple.com",
-  },
+    title: "CineStream – Explore What the World Is Watching",
+    des: "CineStream is your one-stop destination to explore trending movies and TV series from around the world. Watch official trailers, browse by genre, search titles, and discover cast details—all powered by MovieDB.",
+    img: "/cineStream.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/js.svg", "/material-ui.svg", "/redux.svg"],
+    link: "https://cine-stream-one.vercel.app/",
+  }
 ];
 
 export const testimonials = [
