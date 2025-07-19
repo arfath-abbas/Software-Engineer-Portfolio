645bf44cae65499d8c74cb4758a315b92b6f4621
Arfath <arfathabbas.abbas@bt.com>
2025-07-19 20:20:04 +0530

    description fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Arfath's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  description: "Arfath Abbas Software Engineer's Portfolio",
 };
 
 export default function RootLayout({
